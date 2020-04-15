66eee995ece1e21c7a973004700baf82acb8e9a6
elsene <magenelec@gmail.com>
2020-04-15 12:26:08 +0200

    done

--- src/main/java/com/scholanova/ecommerce/cart/service/CartService.java
+++ src/main/java/com/scholanova/ecommerce/cart/service/CartService.java
@@ -4,8 +4,6 @@ import com.scholanova.ecommerce.cart.entity.Cart;
 import com.scholanova.ecommerce.cart.exception.CartException;
 
 public interface CartService {
-
-    public Cart addProductToCart(Cart cart, Long productId, int quantity) throws CartException;
-
+    
     public Cart changeProductQuantity(Cart cart, Long productId, int quantity) throws CartException;
 }
